d388864eed706e8cfe9dbddea47cb4bde5b9bb75
MAFS <martin117h@gmail.com>
2021-10-16 15:42:43 -0500

    se termina

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
+require('dotenv').config();
 const io = require('socket.io')(http, {
     cors: {
         origin: "*",
@@ -10,17 +11,7 @@ app.set('PORT', process.env.PORT || 3000);
 app.use( express.json() );
 app.use( express.urlencoded({extended: false, limit: 1000000000}) );
 
-// require('./socket')(io);
-io.on("connection", (socket) => {
-    console.log("Un cliente se ha conectado");
-    socket.on("message", (data) => {
-        console.log('data :>> ', data)
-        socket.broadcast.emit('todos', { data });
-    });
-    socket.on("messageChat", (data) => {
-        console.log('data :>> ', data)
-        socket.broadcast.emit('todosChat', { data });
-    });
-});
+require('./socket')(io);
+
 
 http.listen(3000, () => console.log(`server on port ${app.get('PORT')}`));
--- socket.js
+++ socket.js
@@ -1,6 +1,13 @@
 module.exports = (io) => {
-    io.on("connection", function (socket) {
-        console.log("Un cliente se ha conectado");
-        socket.emit("messages", messages);
+    io.on("connection", (socket) => {
+    console.log("Un cliente se ha conectado");
+    socket.on("message", (data) => {
+        console.log('data :>> ', data)
+        socket.broadcast.emit('todos', { data });
     });
+    socket.on("messageChat", (data) => {
+        console.log('data :>> ', data)
+        socket.broadcast.emit('todosChat', { data });
+    });
+});
 }
