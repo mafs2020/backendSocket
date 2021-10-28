44eae911e13fc7862c75ba48e4c67dd93617dca1
MAFS <martin117h@gmail.com>
2021-10-27 23:02:01 -0500

    d

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-const cors = require('cors');
+// const cors = require('cors');
 require('dotenv').config();
 require('./db/conexion');
 
 app.set('PORT', process.env.PORT || 3000);
 app.use( express.json() );
 app.use( express.urlencoded({extended: false, limit: 1000000000}) );
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 app.use(cors('*'));
 app.use('/users', require('./routes/userRoute'));
 
